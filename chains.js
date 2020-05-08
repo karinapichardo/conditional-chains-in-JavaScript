fd64a8a3602a77a4f434e20543ef640d4012d1b9
Karina Pichardo <pichardokarina21@yahoo.com>
2020-05-08 15:53:27 -0400

    Update chains.js

--- chains.js
+++ chains.js
@@ -5,7 +5,7 @@ If none of the operations can give 24, return null.
 */
 
 function operation(num1, num2) {
-  return (num1 + num2 === 24) ? 'added'
+    return (num1 + num2 === 24) ? 'added'
          : (num1 - num2 === 24) ? 'subtracted'
          : (num1 * num2 === 24) ? 'multiplied'
          : (num1 / num2 === 24) ? 'divided'
